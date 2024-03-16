ca1cd008e92fd4a1fd9d1b952433835adc0b71f1
Школьник01 <Kids01@edu.misis.ru>
2024-03-16 12:38:04 +0300

    final

--- 5/problem_5.py
+++ 5/problem_5.py
@@ -23,22 +23,15 @@ def reader(route, d):
 
 def main():
     header, data = reader("../monster_game.txt", "$")
-    ability = {"усиление атаки": "attack", "регенерация":  "health", "дополнительный ход": "speed"}
-    monsters_exp = []
+    sett = {}
     for monster in data:
-        exp = 0
-        for r in ['attack', 'health',  'speed', "protection"]:
-            num = float(monster[r])
-            mult = 1
-            to_ab = ability[monster["opportunity"]]
-            if to_ab == r:
-                mult = 1.5
-            exp = exp + mult*num
-        corrected_monster = []
-        corrected_monster = [monster["MonsterName"], exp]
-        monsters_exp.append(corrected_monster)
-    for i in range(10):
-        print(monsters_exp[i][0]+":"+str(monsters_exp[i][1]))
+        if monster["MonsterName"] not in sett:
+            sett[monster["MonsterName"]] = 0
+        sett[monster["MonsterName"]] += float(monster["attack"])
+    names = list(sett.keys())[:10]
+    for n in names:
+        print(n, sett[n])
+
 
 
 
